798d08807b592dbe3238132cb2f0dfc8522a8419
Sven Weidauer <sven.weidauer@gmail.com>
2010-12-15 13:17:10 +0100

    Testing assertions in CParserToken

--- Tests/TestCParserToken.m
+++ Tests/TestCParserToken.m
@@ -22,6 +22,11 @@ static NSString * const TestMacro = @"#macro";
 	CParserToken *token = [CParserToken tokenWithNumber: TestNumber];
 	STAssertEquals( [token type], CParserTokenNumber, @"Wrong type" );
 	STAssertEquals( [token numberValue], TestNumber, @"Wrong number" );
+	
+	STAssertThrows( [token operatorValue], @"No operator" );
+	STAssertThrows( [token functionValue], @"No function" );
+	STAssertThrows( [token variableValue], @"No variable" );
+	STAssertThrows( [token macroValue], @"No macro" );
 }
 
 - (void) testOperatorToken;
@@ -29,6 +34,11 @@ static NSString * const TestMacro = @"#macro";
 	CParserToken *token = [CParserToken tokenWithOperator: TestOperator];
 	STAssertEquals( [token type], CParserTokenOperator, @"Wrong type" );
 	STAssertEqualObjects( [token operatorValue], TestOperator, @"Wrong operator" );
+
+	STAssertThrows( [token numberValue], @"No number" );
+	STAssertThrows( [token functionValue], @"No function" );
+	STAssertThrows( [token variableValue], @"No variable" );
+	STAssertThrows( [token macroValue], @"No macro" );
 }
 
 - (void) testFunctionToken;
@@ -36,6 +46,11 @@ static NSString * const TestMacro = @"#macro";
 	CParserToken *token = [CParserToken tokenWithFunction: TestFunction];
 	STAssertEquals( [token type], CParserTokenFunction, @"Wrong type" );
 	STAssertEqualObjects( [token functionValue], TestFunction, @"Wrong function" );
+
+	STAssertThrows( [token numberValue], @"No number" );
+	STAssertThrows( [token operatorValue], @"No operator" );
+	STAssertThrows( [token variableValue], @"No variable" );
+	STAssertThrows( [token macroValue], @"No macro" );
 }
 
 - (void) testVariableToken;
@@ -43,6 +58,18 @@ static NSString * const TestMacro = @"#macro";
 	CParserToken *token = [CParserToken tokenWithVariable: TestVariable];
 	STAssertEquals( [token type], CParserTokenVariable, @"Wrong type" );
 	STAssertEqualObjects( [token variableValue], TestVariable, @"Wrong variable" );
+	
+	STAssertTrueNoThrow( [token numberValue] == 0.0, @"Should have number 0" );
+	
+	[token setNumberValue: TestNumber];
+	STAssertEquals( [token type], CParserTokenVariable, @"Wrong type" );
+	STAssertEqualObjects( [token variableValue], TestVariable, @"Wrong variable" );
+	STAssertEquals( [token numberValue], TestNumber, @"Wrong number" );
+
+	STAssertThrows( [token operatorValue], @"No operator" );
+	STAssertThrows( [token functionValue], @"No function" );
+	STAssertThrows( [token macroValue], @"No macro" );
+	
 }
 
 - (void) testMacroToken;
@@ -50,6 +77,11 @@ static NSString * const TestMacro = @"#macro";
 	CParserToken *token = [CParserToken tokenWithMacro: TestMacro];
 	STAssertEquals( [token type], CParserTokenMacro, @"Wrong type" );
 	STAssertEqualObjects( [token macroValue], TestMacro, @"Wrong macro" );
+
+	STAssertThrows( [token numberValue], @"No number" );
+	STAssertThrows( [token operatorValue], @"No operator" );
+	STAssertThrows( [token functionValue], @"No function" );
+	STAssertThrows( [token variableValue], @"No variable" );
 }
 
 @end
