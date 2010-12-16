df6f61c20db376a80bb373229ae5af4471bb9305
Sven Weidauer <sven.weidauer@gmail.com>
2010-12-16 08:08:21 +0100

    Fixed wrong merge of NSArray+reverse
    Conflicts:
    	NSArray+reverse.h
    	NSArray+reverse.m

--- NSArray+reverse.h
+++ NSArray+reverse.h
@@ -3,19 +3,11 @@
 //  CMathParser
 //
 //  Created by Johannes Wolf on 27.09.10.
-<<<<<<< HEAD
 //  Copyright 2010 beanage. All rights reserved.
-=======
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
->>>>>>> refs/remotes/old/master
 //
 
 #import <Cocoa/Cocoa.h>
 
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/old/master
 @interface NSArray (reverse)
 
 - (NSArray *) reversedArray;
@@ -27,8 +19,4 @@
 
 - (void) reverse;
 
-<<<<<<< HEAD
 @end
-=======
-@end
->>>>>>> refs/remotes/old/master
--- NSArray+reverse.m
+++ NSArray+reverse.m
@@ -3,19 +3,11 @@
 //  CMathParser
 //
 //  Created by Johannes Wolf on 27.09.10.
-<<<<<<< HEAD
 //  Copyright 2010 beanage. All rights reserved.
 //
 
 #import "NSArray+reverse.h"
 
-
-=======
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
-//
-
-#import "NSArray+reverse.h"
->>>>>>> refs/remotes/old/master
 @implementation NSArray (reverse)
 
 - (NSArray *) reversedArray;
