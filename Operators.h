bd17b79c81a8ad1069d1bd6560f5efd609cb5178
Sven Weidauer <sven.weidauer@gmail.com>
2010-12-17 10:37:43 +0100

    Using preprocessor magic to create table of operators

---
+++ Operators.h
@@ -0,0 +1,66 @@
+#define ALL_OPERATORS(def)	\
+	/*		Name		Token	Associativity	Priority	Arguments	*/ \
+	def(	Plus,		@"+",	Left,			6,			2			) \
+	def(	Minus,		@"-",	Left,			6,			2			) \
+	def(	Neg,		nil,	Left,			8,			1			) \
+	def(	Times,		@"*",	Left,			7,			2			) \
+	def(	Div,		@"/",	Left,			7,			2			) \
+	def(	Modulo,		@"%",	Left,			7,			2			) \
+	def(	Equal,		@"==",	Left,			4,			2			) \
+	def(	Assign,		@"=",	Right,			2,			2			) \
+	def(	LBrace,		@"(",	None,			0,			0			) \
+	def(	RBrace,		@")",	None,			0,			0			) \
+	def(	Semicolon,	@";",	None,			0,			0			) \
+	def(	Comma,		@",",	Left,			1,			0			) \
+	def(	Power,		@"^",	Left,			9,			2			) \
+	def(	LE,			@"<=",	Left,			5,			2			) \
+	def(	LT,			@"<",	Left,			5,			2			) \
+	def(	GE,			@">=",	Left,			5,			2			) \
+	def(	GT,			@">",	Left,			5,			2			) \
+	def(	NEqual,		@"!=",	Left,			4,			2			) \
+	def(	Factorial,	@"!",	Right,			8,			1			) \
+	def(	And,		@"&&",	Left,			3,			2			) \
+	def(	Or,			@"||",	Left,			3,			2			)
+
+
+typedef enum CPOperator CPOperator;
+
+typedef enum CPOperatorAssoc {
+	CPOperatorAssocNone = 0,
+	CPOperatorAssocLeft,
+	CPOperatorAssocRight
+} CPOperatorAssoc;
+
+
+@interface Operators : NSObject
+
++ (CPOperatorAssoc) associativity: (CPOperator) op;
++ (int)	priority: (CPOperator) op;
++ (unsigned) argumentCount: (CPOperator) op;
++ (CPOperator) scan: (NSScanner *) scanner;
+
+@end
+
+
+/*****
+ * Ignore everything below this line
+ ************************************/
+
+#define	ASSOC( name )	CPOperatorAssoc ## name
+#define OPERATOR( name ) CPOperator ## name 
+
+#define DEF_ENUM( a, b, c, d, e )	OPERATOR(a),
+
+enum CPOperator {
+	OPERATOR( Null ) = 0,
+	ALL_OPERATORS( DEF_ENUM )
+	CPOperatorCount
+};
+
+#undef DEF_ENUM
+
+#if !defined( _COMPILE_OPERATORS )
+#	undef ALL_OPERATORS
+#	undef OPERATOR
+#	undef ASSOC
+#endif
---
+++ Operators.m
@@ -0,0 +1,58 @@
+#define _COMPILE_OPERATORS
+#import "Operators.h"
+
+@implementation Operators
+
+typedef struct OperatorInfo  {
+	CPOperatorAssoc associativity;
+	NSString *token;
+	int priority;
+	unsigned args;
+} OperatorInfo;
+
+
+#define DEF_STRUCT( _name, _token, _assoc, _priority, _args )	\
+	[OPERATOR(_name)] = {										\
+		.token = _token,										\
+		.associativity = ASSOC( _assoc ),						\
+		.priority = _priority,									\
+		.args = _args											\
+	},
+
+
+static const OperatorInfo allOperators[CPOperatorCount] = {
+	ALL_OPERATORS( DEF_STRUCT )
+};
+
+#undef DEF_STRUCT
+
++ (CPOperatorAssoc) associativity: (CPOperator) op;
+{
+	NSParameterAssert( CPOperatorNull < op && op < CPOperatorCount );
+	return allOperators[op].associativity;
+}
+
++ (int)	priority: (CPOperator) op;
+{
+	NSParameterAssert( CPOperatorNull < op && op < CPOperatorCount );
+	return allOperators[op].priority;
+}
+
++ (unsigned) argumentCount: (CPOperator) op;
+{
+	NSParameterAssert( CPOperatorNull < op && op < CPOperatorCount );
+	return allOperators[op].args;
+}
+
++ (CPOperator) scan: (NSScanner *) scanner;
+{
+	for (unsigned i = 1; i < CPOperatorCount; i++) {
+		NSString *token = allOperators[i].token;
+		if (nil != token && [scanner scanString: token intoString: NULL]) {
+			return i;
+		}
+	}
+	return CPOperatorNull;
+}
+
+@end
